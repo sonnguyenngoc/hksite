f49bfc196718528dfaaf2410e9906f632fc5ae96
Nguyen Ton Hung <hungnt@hoangkhang.com.vn>
2016-01-18 23:57:33 +0700

    chuc nang ngay 18-1-2016

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   def index
     @class_body = "front_page"
     @contact = Contact.find(1)
-    @product = Product.order("created_at DESC").first(15)
+    @product = Product.order("created_at DESC").first(12)
+    @product_image = ProductImage.order("created_at DESC").first(1)
+    @product_price = ProductPrice.order("created_at DESC").first(12)
   end
 end
