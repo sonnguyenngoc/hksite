fb3b224033419c51f61a0373863ae565ab34e17b
sonnguyenngoc0811 <sonnguyenngoc0811@gmail.com>
2016-02-18 15:01:21 +0700

    new version

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -4,6 +4,7 @@ class BlogController < ApplicationController
     @message = Message.new
     @company_info = Contact.find(1)
     @blog = Article.order("created_at DESC")
+    @tag = Tag.order("created_at DESC")
     @new_blog_footer = Article.order("created_at DESC").first(3)
   end
 end
--- app/controllers/blog_post_controller.rb
+++ app/controllers/blog_post_controller.rb
@@ -5,6 +5,7 @@ class BlogPostController < ApplicationController
     @company_info = Contact.find(1)
     @blog = Article.order("created_at DESC").first(3)
     @blog_post = Article.find(params[:id])
+    @tag = Tag.order("created_at DESC")
     @new_blog_footer = Article.order("created_at DESC").first(3)
   end
 end
