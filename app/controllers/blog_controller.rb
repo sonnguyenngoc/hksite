a4868eefdada21af26e9b69620bf2983f68d7102
sonnguyenngoc0811 <sonnguyenngoc0811@gmail.com>
2016-02-25 16:26:59 +0700

    update code hksite 25022016

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,7 +3,7 @@ class BlogController < ApplicationController
     @menu = Menu.all
     @message = Message.new
     @company_info = Contact.find(1)
-    @blog = Article.paginate(:page => params[:page], :per_page => 2).order("created_at DESC")
+    @blog = Article.paginate(:page => params[:page], :per_page => 3).order("created_at DESC")
     @tag = Tag.order("created_at DESC")
     @new_blog_footer = Article.order("created_at DESC").first(3)
   end
