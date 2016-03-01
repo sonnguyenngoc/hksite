94977564cd011cd6a47ff0fe64256f87d85e47d9
sonnguyenngoc0811 <sonnguyenngoc0811@gmail.com>
2016-03-01 10:33:17 +0700

    fix partners logo small

--- app/controllers/about_us_controller.rb
+++ app/controllers/about_us_controller.rb
@@ -4,6 +4,6 @@ class AboutUsController < ApplicationController
     @message = Message.new
     @company_info = Contact.find(1)
     @about_us = Article.joins(:tag).where(tags: {title: "Giới thiệu công ty"}).first(1)
-    @new_blog_footer = Article.order("created_at DESC").first(3)
+    @new_blog_footer = Article.get_all_articles.first(3)
   end
 end
