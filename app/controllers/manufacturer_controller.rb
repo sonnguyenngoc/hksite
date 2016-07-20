b46d70d5ccf9994d568b49927890f72d21916605
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-07-19 21:02:51 -0700

    fix bug hien thi tin moi nhat

--- app/controllers/manufacturer_controller.rb
+++ app/controllers/manufacturer_controller.rb
@@ -4,6 +4,6 @@ class ManufacturerController < ApplicationController
       @message = Message.new
       @company_info = Contact.find(1)
       @partners = Partner.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
-      @new_blog_footer = Article.order("created_at DESC").first(3)
+      @new_blog_footer = Article.get_all_articles.first(3)
   end
 end
