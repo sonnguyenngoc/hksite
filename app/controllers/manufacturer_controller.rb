6ef081f3b9c0efeb4184281e90925d912b1ca0f1
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-07-21 21:57:32 -0700

    cap nhat left bar manufacturer + display product limit

--- app/controllers/manufacturer_controller.rb
+++ app/controllers/manufacturer_controller.rb
@@ -3,7 +3,7 @@ class ManufacturerController < ApplicationController
       @menu = Menu.all
       @message = Message.new
       @company_info = Contact.find(1)
-      @partners = Partner.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
+      @partners = Partner.order("created_at DESC").paginate(:page => params[:page], :per_page => 20)
       @new_blog_footer = Article.get_all_articles.first(3)
   end
 end
