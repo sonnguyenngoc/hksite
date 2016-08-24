6b77f2f5e94f86538b4f6a7f03f8016d1f618718
hungtnguyen263 <hungnt@hoangkhang.com.vn>
2016-08-24 01:03:37 -0700

    fix security for account

--- app/controllers/infomations_controller.rb
+++ app/controllers/infomations_controller.rb
@@ -3,7 +3,6 @@ class InfomationsController < ApplicationController
     @menu = Menu.all
     @message = Message.new
     @company_info = Contact.find(1)
-    #@about_us = Article.joins(:tag).where(tags: {title: "Giới thiệu công ty"}).first(1)
     @policy = Article.joins(:tag).where(tags: {title: "Chính sách quy định - Điều khoản & điều kiện"}).first(1)
     @new_blog_footer = Article.get_all_articles.first(3)
   end
