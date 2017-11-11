1e17704fa173439fafa42abdc5f2d89f911571e7
Luan Pham <luanpm88@gmail.com>
2017-11-10 18:38:41 -0800

    quick search top #desktop

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,42 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
   root 'home#index'
-  
+
   get "tim-kiem-san-pham" => "product_search#index", as: :product_search
   get "san-pham(/:title)-:id.html" => "product#index", as: :product
   get "chuyen-muc(/:title)-:id.html" => "category_page#index", as: :category
   get "hinh-anh(/:title)(/:id)/(:type)" => "product_images#image", as: :product_image
-  
+
   get "tin-tuc.html" => "blog#index", as: :blog_listing
   get "tin-tuc/chi-tiet.html" => "blog_detail#index", as: :blog_detail
   get "blog_post" => "blog_post#index", as: :blog_post
-  
+
   get "lien-he.html" => "contact#index", as: :contact
   get "gioi-thieu.html" => "about_us#index", as: :about_us
-  
+
   get "gio-hang.html" => "shopping_cart#index", as: :shopping_cart
   get "dat-hang.html" => "check_out#index", as: :check_out
   get "dat-hang/thanh-cong.html" => "finish_checkout#index", as: :finish_checkout
-  
+
   get "infomations/policy" => "infomations#policy", as: :policy
   get "manufacturer" => "manufacturer#index", as: :manufacturer
   get "manufacturer_page" => "manufacturer_page#index", as: :manufacturer_page
-  
+
   get "all_bestseller_product" => "all_bestseller_product#index", as: :all_bestseller_product
   get "all_hot_product" => "all_hot_product#index", as: :all_hot_product
   get "all_new_product" => "all_new_product#index", as: :all_new_product
   get "all_prominent_product" => "all_prominent_product#index", as: :all_prominent_product
   get "all_sale_product" => "all_sale_product#index", as: :all_sale_product
-  
+
+  #@todo online store
+	get "autosearch" => "product#autosearch", as: :autosearch
+
   devise_for :logins
   resources :customer_orders
   resources :line_items
   resources :carts
   resources :category
   resources :product
-  
+
   namespace :admin, :path => "hoangkhangincotech" do
       resources :partners
       resources :articles
@@ -46,5 +49,5 @@ Rails.application.routes.draw do
       resources :customer_orders
       resources :product_infos
       get "old_category" => "old_category#index", as: :old_category
-  end  
+  end
 end
---
+++ public/hksite/js/main.js
@@ -0,0 +1,133 @@
+function autoSearchMoveUp(box) {
+    var current_li = box.find('li.current');
+    var prev;
+    if (current_li.length) {
+        prev = current_li.prev();
+    } else {
+        prev = box.find('li').last();
+    }
+
+    box.find('li').removeClass('current');
+
+    if (prev.length) {
+        prev.addClass('current');
+    } else{
+        current_li.parents('ul').find('li').last().addClass('current');
+    }
+}
+
+function autoSearchMoveDown(box) {
+    var current_li = box.find('li.current');
+    var next;
+    if (current_li.length) {
+        next = current_li.next();
+    } else {
+        next = box.find('li').first();
+    }
+
+
+    box.find('li').removeClass('current');
+
+    if (next.length) {
+        next.addClass('current');
+    } else{
+        next.parents('ul').find('li').eq(0).addClass('current');
+    }
+}
+
+var autosearch_xhr;
+function autoSearch(box) {
+    var input = box.find('.autosearch-input');
+    var keyword = input.val();
+    var url = box.attr('data-url');
+    var result_box = box.find('.autosearch-result-box ul');
+    var menu_id = box.find('select').val();
+
+    // if keyword == ''
+    if (keyword.trim() == '') {
+        result_box.parent().hide();
+        return;
+    }
+
+    if (!box.find('.autosearch-result-box').length) {
+        box.append('<div class="autosearch-result-box"><ul></ul></div>');
+        var result_box = box.find('.autosearch-result-box ul');
+    }
+
+    // ajax autosearch
+	if(autosearch_xhr && autosearch_xhr.readyState != 4){
+		autosearch_xhr.abort();
+	}
+    autosearch_xhr = $.ajax({
+        url: url,
+        data: {
+            keyword: keyword,
+            menu_id: menu_id
+        }
+    }).done(function( items ) {
+        result_box.html('');
+        result_box.parent().show();
+        items.forEach(function(item) {
+            result_box.append(
+                '<li><a title="' + item.name + '" href="' + item.link + '">' +
+                    '<img src="' +item.image+ '" />' +
+                    '<div><span class="title">' + item.name + '</span>' +
+                    '<span class="price sold_out_' +item.is_sold_out+ '">' + (item.is_sold_out ? 'Giá liên hệ' : item.price) + '</span>' +
+                    ' <span class="old_price old_price_'+item.old_price+'"><span class="num">' + item.old_price + '</span></span>' +
+                '</div></a></li>'
+            );
+        });
+        if (items.length) {
+            // result_box.find('li').eq(0).addClass('current');
+        } else {
+            result_box.append(
+                '<li class="autosearch-empty-line">Không có sản phẩm phù hợp</li>'
+            );
+        }
+    });
+}
+
+$(document).ready(function () {
+    $(document).on('keyup', '.autosearch-input', function(e) {
+        var box = $(this).parents('.autosearch');
+        var form = $(this).parents('form');
+
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if (code === 38) {
+            autoSearchMoveUp(box);
+            return;
+        }
+        if (code === 40) {
+            autoSearchMoveDown(box);
+            return;
+        }
+        if (code === 13) {
+            if (box.find('li.current a').length) {
+                var link = box.find('li.current a').attr('href');
+                window.location = link;
+                return;
+            } else {
+                form.submit();
+            }
+        }
+        autoSearch(box);
+    });
+
+    $(document).on('change', '.autosearch select', function(e) {
+        var box = $(this).parents('.autosearch');
+
+        autoSearch(box);
+    });
+
+    $(document).on('focusout', '.autosearch-input', function() {
+        var box = $(this).parents('.autosearch');
+        setTimeout(function() {
+            box.find('.autosearch-result-box').hide();
+        }, 500);
+    });
+
+    $(document).on('focusin', '.autosearch-input', function() {
+        var box = $(this).parents('.autosearch');
+        box.find('.autosearch-result-box').show();
+    });
+});
