058e7a9d0fa4b42bd035db67e37a7995f276097a
Luan Pham <luanpm88@gmail.com>
2017-11-12 19:27:11 -0800

    fix ajax box js

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,24 @@
 class HomeController < ApplicationController
   def index
   end
-  
+
   def hot_products
+    render layout: nil
   end
-  
+
   def stock_inventory
+    render layout: nil
   end
-  
+
   def promotion_banner
+    render layout: nil
   end
-  
+
   def bestselling
+    render layout: nil
   end
-  
+
   def listing_brand
+    render layout: nil
   end
 end
