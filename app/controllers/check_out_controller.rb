364aa9134a5bc0e1f37ee1eb8eb979bcf157af94
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-11-16 01:33:45 -0800

    coding finish checkout page

--- app/controllers/check_out_controller.rb
+++ app/controllers/check_out_controller.rb
@@ -2,5 +2,10 @@ class CheckOutController < ApplicationController
   def index
     @menus = Menu.get_menus
     @customer_order = CustomerOrder.new
+    
+    if @cart.line_items.empty?
+      redirect_to shopping_cart_path
+      return
+    end
   end
 end
