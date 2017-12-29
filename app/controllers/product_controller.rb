ba921d9afd917ea9c4f42f157a2d2b4951e4a804
Luan Pham <luanpm88@gmail.com>
2017-12-29 14:39:24 +0700

    fix autosearch old price

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -20,7 +20,7 @@ class ProductController < ApplicationController
       name: product.display_name,
       price: (!product.has_price ? 'Liên hệ' : (view_context.number_to_currency(product.display_custom_price, precision: 0, format: "%n₫", separator: ',', unit: '', delimiter: "."))),
       #is_deal: product.is_deal,
-      old_price: (!product.get_old_price.nil? ? view_context.number_with_delimiter(product.get_old_price.price.to_i) + "₫" : 'null'),
+      old_price: (product.check_product_sale ? view_context.format_price(product.product_info.old_price) + "₫" : 'null'),
       #deal_percent: (product.deal_percent if product.is_deal),
       link: view_context.url_for({controller: "product", action: "index", id: product.id, title: url_friendly(view_context.strip_tags(product.display_name))}),
       #is_sold_out: product.is_sold_out,
