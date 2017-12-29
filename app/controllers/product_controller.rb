3756ccff623c0b812719f83840a6162164775c7a
Luan Pham <luanpm88@gmail.com>
2017-12-29 16:00:53 +0700

    auto search layout

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -18,9 +18,9 @@ class ProductController < ApplicationController
 
     render json: @products.map { |product| {
       name: product.display_name,
-      price: (!product.has_price ? 'Liên hệ' : (view_context.number_to_currency(product.display_custom_price, precision: 0, format: "%n₫", separator: ',', unit: '', delimiter: "."))),
+      price: (!product.has_price ? 'Liên hệ' : (view_context.format_price(product.display_custom_price))),
       #is_deal: product.is_deal,
-      old_price: (product.check_product_sale ? view_context.format_price(product.product_info.old_price) + "₫" : 'null'),
+      old_price: (product.check_product_sale ? view_context.format_price(product.product_info.old_price) : 'null'),
       #deal_percent: (product.deal_percent if product.is_deal),
       link: view_context.url_for({controller: "product", action: "index", id: product.id, title: url_friendly(view_context.strip_tags(product.display_name))}),
       #is_sold_out: product.is_sold_out,
